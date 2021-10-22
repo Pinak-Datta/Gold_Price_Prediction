ef412fc02c983057fb81a75478c0487058b6abb5
Pinak Datta <87294070+Pinak-Datta@users.noreply.github.com>
2021-10-22 10:02:52 +0530

    Create Gold_Price_Pred.py

---
+++ Gold_Price_Pred.py
@@ -0,0 +1,10 @@
+# Importing dependencies
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn import metrics
+
+# Data collection and preprocessing
