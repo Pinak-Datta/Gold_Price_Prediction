237d552be4e31fe977d6e7515855bc40547ae86f
Pinak Datta <87294070+Pinak-Datta@users.noreply.github.com>
2021-10-22 10:04:04 +0530

    Update Gold_Price_Pred.py

--- Gold_Price_Pred.py
+++ Gold_Price_Pred.py
@@ -7,4 +7,15 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import metrics
 
+
 # Data collection and preprocessing
+
+gold_data = pd.read_csv('/content/gold price dataset.csv')
+gold_data.head()
+gold_data.isnull().sum()
+
+
+#Splitting the dataset into target and features
+
+X = gold_data.drop(['Date','GLD'],axis=1)
+Y = gold_data['GLD']
